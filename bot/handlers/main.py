464aa1faecf22012a49aa41c92dcbc1ddb0445bc
ilia <froloviliya.best@yandex.ru>
2023-02-25 13:56:55 +0300

    help command for superadmin added

--- bot/handlers/main.py
+++ bot/handlers/main.py
@@ -5,6 +5,7 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 from utils import keyboards, request_funcs
 from bot_run import bot
 from utils.lang_parser import get_phrase
+from utils.check_role import super_admin_require
 
 
 async def cancel_handler(message: types.Message, state: FSMContext) -> None:
@@ -39,6 +40,18 @@ async def greeting(message: types.Message, state: FSMContext) -> None:
         await bot.send_message(message.from_user.id, get_phrase('ask_personal_agreement'),
                                reply_markup=keyboards.REGISTRATION_KEYBOARD)
     await message.delete()
+
+
+@super_admin_require
+async def super_admin_help(message: types.Message, state: FSMContext):
+    """ Отлавливает "/help" от супер-админа, выдаёт список команд. """
+
+    current_state = await state.get_state()
+    if current_state is not None:
+        await state.finish()
+
+    await bot.send_message(message.from_user.id, get_phrase('superadmin_help'),
+                           reply_markup=await keyboards.keyboard_choice(message.from_user.id))
 
 
 class RegistrationFSM(StatesGroup):
@@ -81,6 +94,7 @@ def register_main_handlers(dp: Dispatcher) -> None:
     """Регистрация основных хендлеров."""
     dp.register_message_handler(cancel_handler, filters.Text(equals='отмена', ignore_case=True), state='*')
     dp.register_message_handler(greeting, commands=["start"], state="*")
+    dp.register_message_handler(super_admin_help, commands=['help'], state="*")
     dp.register_message_handler(registration, text=['Регистрация'])
     dp.register_message_handler(obtain_stud_info, content_types=['text'],
                                 state=RegistrationFSM.waiting_stud_info)
